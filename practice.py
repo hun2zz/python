35a7938ec83cfb80e23a48adf03bcc5a028f1f72
hun2zz <tkdgnsdldkdlel@naver.com>
2024-05-15 19:53:46 +0900

    ✅ 파이써 기초 수강

--- practice.py
+++ practice.py
@@ -1,1 +1,19 @@
-print('hello world')
+print(True)
+
+
+envelope1 = 100000
+envelope2 = 200000
+envelope3 = 300000
+
+print(envelope1);
+
+# 변수 이름 규칙
+"""1. 문자 또는 _로 시작 
+ 2, 문자, 숫자 , _로 구성
+ 3. 공백  x, 특수문자 x  """
+
+# 파이썬의 주석은 한줄은 # 여러줄은 """ 로 구분함.
+
+print(5 < 3 or 3 < 5) # true 반환 파이썬에는 || && 가 아니라 and ,or 로 작성.
+
+print('f' in 'cat') # f라는 글자가 cat이라는 글자안에 있는지 확인 할 수 있음
